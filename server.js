834cd09e98dfcded4da281c19a5fd28d7fcf7a9f
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2020-08-14 00:45:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
-
-
 //user configurable items
 let prefix = "!";
-let token = "NjYwMTI3Mjg0MjI0NzIwODk3.XgYVcQ.axA6ILg0t0j1y8kN4jysWoY5r5c";
+let token = "redacted";
 
 //imports
 const fetch = require("node-fetch");
